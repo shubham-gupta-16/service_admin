59eaf0a8ee00b03f62009844f9cb714fa384d101
Shubham Gupta <shubham.developer16@gmail.com>
2022-10-27 20:23:04 +0530

    Selected device animation done

--- lib/ui/pages/device_page.dart
+++ lib/ui/pages/device_page.dart
@@ -5,6 +5,7 @@ import '../sections/device_section.dart';
 
 class DevicePage extends StatefulWidget {
   final bool isDesktop;
+
   const DevicePage({Key? key, required this.isDesktop}) : super(key: key);
 
   @override
@@ -14,7 +15,6 @@ class DevicePage extends StatefulWidget {
 class _DevicePageState extends State<DevicePage> {
   late DeviceDataConnection _dataConnection;
   late TextEditingController _textEditingController;
-
 
   @override
   void initState() {
@@ -32,14 +32,11 @@ class _DevicePageState extends State<DevicePage> {
   ///////////////////////// FRAGMENT STATE /////////////////////////
   DeviceFragment? fragment;
 
-  void switchFragment(DeviceFragment fragment){
-    ModalRoute.of(context)?.addLocalHistoryEntry(
-        LocalHistoryEntry(
-            onRemove: () {
-              setState(() => this.fragment = null);
-            }
-        )
-    );
+  void switchFragment(DeviceFragment fragment) {
+    ModalRoute.of(context)
+        ?.addLocalHistoryEntry(LocalHistoryEntry(onRemove: () {
+      setState(() => this.fragment = null);
+    }));
     setState(() {
       this.fragment = fragment;
     });
@@ -48,58 +45,70 @@ class _DevicePageState extends State<DevicePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Column(
-        children: [
-          Expanded(
-            child: Stack(
-              children: [
-                DeviceSection(onCardPressed: (fragment) {
-                  switchFragment(fragment);
-                },),
-                AnimatedSwitcher(duration: const Duration(milliseconds: 300),
-                transitionBuilder: (child, animation) {
-                  print(animation);
-                  return ScaleTransition(scale: animation,
-                  child: FadeTransition(opacity: animation, child: child,));
-                },
-                child: fragment?.widget ?? const SizedBox(),)
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _textEditingController,
-                    decoration: InputDecoration(
-                        contentPadding:
-                        const EdgeInsets.symmetric(horizontal: 10),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none),
-                        fillColor:
-                        Theme.of(context).colorScheme.secondaryContainer,
-                        filled: true,
-                        hintText: "Command"),
+        resizeToAvoidBottomInset: true,
+        body: Column(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  AnimatedScale(
+                    scale: fragment != null ? 1.05 : 1,
+                    duration: const Duration(milliseconds: 160),
+                    child: DeviceSection(
+                      onCardPressed: (fragment) {
+                        switchFragment(fragment);
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                IconButton(
-                    onPressed: () {
-                      _dataConnection
-                          .runCommand(_textEditingController.text.trim());
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 160),
+                    transitionBuilder: (child, animation) {
+                      print(animation);
+                      return FadeTransition(
+                        opacity: animation,
+                        child: ScaleTransition(
+                            scale: Tween<double>(begin: 0.8, end: 1)
+                                .animate(animation),
+                            child: child),
+                      );
                     },
-                    icon: const Icon(Icons.send))
-              ],
+                    child: fragment?.widget ?? const SizedBox(),
+                  )
+                ],
+              ),
             ),
-          )
-        ],
-      )
-    );
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 10),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none),
+                          fillColor:
+                              Theme.of(context).colorScheme.secondaryContainer,
+                          filled: true,
+                          hintText: "Command"),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        _dataConnection
+                            .runCommand(_textEditingController.text.trim());
+                      },
+                      icon: const Icon(Icons.send))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
